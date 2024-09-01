2e897a531c8ba97be6a627e66a446d8477120ccc
Joshmatjjen <joshmatparrot@gmail.com>
2024-09-01 21:44:07 +0100

    Fix bugs on app

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, Suspense } from "react";
 import Confetti from "react-confetti";
 
-const SuccessPage = () => {
+const OrderIdDisplay = () => {
   const searchParams = useSearchParams();
   const router = useRouter();
 
@@ -30,6 +30,14 @@ const SuccessPage = () => {
       </h2>
       <h3 className="">You are being redirected to the order page...</h3>
     </div>
+  );
+};
+
+const SuccessPage = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <OrderIdDisplay />
+    </Suspense>
   );
 };
 
